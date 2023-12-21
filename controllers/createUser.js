5cd4b9f2315a3ee678dcc4c41d447a8b21ebf31e
Lakshya Sadhwani <lakshyamwork@gmail.com>
2023-12-21 18:34:39 +0530

    app bug

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -2,7 +2,7 @@ const User = require('../models/userSchema');
 
 exports.createUser = async (req, res) => {
     try {
-        const { name, email, phone } = req.body;\
+        const { name, email, phone } = req.body;
             let createEntry;
             phone ?
              createEntry = await User.create({ name, email, phone })
